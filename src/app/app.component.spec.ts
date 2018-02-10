a463ff1bb6eef51d6c656fdb089a4be4c3b930cf
PhilippeMorier <morier.dev@outlook.com>
2018-02-10 11:13:42 +0100

    refactor(app): move `createComponent` into `beforeEach`

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { AgmCoreModule } from '@agm/core';
 import { MdlModule } from '@angular-mdl/core';
-import { async, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
@@ -9,6 +9,8 @@ import { FlightPlansComponent } from './flight-plans/flight-plans.component';
 import { FlightPlansService } from './service/flight-plans.service';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -29,37 +31,34 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
   it(`should have as title 'Drone Flight Planner'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Drone Flight Planner');
   }));
 
   it('should render title', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('mdl-layout-title').textContent)
       .toContain('Drone Flight Planner');
   }));
 
   it(`should render one 'dfp-flight-plans'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelectorAll('dfp-flight-plans').length)
       .toEqual(1);
   }));
 
   it(`should render one 'dfp-flight-editor'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelectorAll('dfp-flight-editor').length)
       .toEqual(1);
