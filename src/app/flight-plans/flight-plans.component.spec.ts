b0f48fa7dfd0755211642192c8da338152d52140
PhilippeMorier <morier.dev@outlook.com>
2018-02-10 09:09:24 +0100

    test(flight-plans): check if all plans get listed

--- src/app/flight-plans/flight-plans.component.spec.ts
+++ src/app/flight-plans/flight-plans.component.spec.ts
@@ -28,4 +28,22 @@ describe('FlightPlansComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render all plans in a list', () => {
+    component.flightPlans = [{
+      coordinates: [],
+      createdOn: new Date(),
+      id: 1,
+      name: 'First Test Plan',
+    }, {
+      coordinates: [],
+      createdOn: new Date(),
+      id: 2,
+      name: 'Second Test Plan',
+    }];
+    fixture.detectChanges();
+
+    expect(fixture.nativeElement.querySelectorAll('li').length)
+      .toEqual(component.flightPlans.length);
+  });
 });
